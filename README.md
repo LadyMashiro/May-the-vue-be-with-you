11ee1be78dcf01445b32c1c1a145a9aa852a407a
Lady Mashiro <caeschler@gmail.com>
2019-10-05 00:29:25 +0200

    updated vuepress site

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
----
-home: true
-heroImage: vue-bunny.png
-heroText: Vue Beginners Workshop
-tagline: Let the vue be with you.
-actionText: Let's begin ➜
-actionLink: /guide/vue
-features:
-- title: a title
-  details: a text
-- title: b title
-  details: b text
-- title: c title
-  details: c text
-footer: footer
----
+# Vuepress Site
+
+My awesome text
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,59 @@
+module.exports = {
+	title: 'Lady Mashiro',
+	head: [
+		['link', { rel: 'icon', href: `/favicon.png` }]
+	],
+	ga: '',
+	description: 'This is your page description.',
+	themeConfig: {
+		// repo: 'https://github.com/bencodezen/vuepress-starter-kit',
+		editLinks: false,
+		editLinkText: 'Edit this page',
+		lastUpdated: 'Last updated',
+		nav: [
+			{
+				text: 'Home',
+				link: '/'
+			},
+			{
+				text: 'Basic Page',
+				link: '/basic/'
+			},
+			{
+				text: 'About',
+				link: '/about/'
+			},
+			{
+				text: 'Component Example',
+				link: '/component-example'
+			},
+			/*{
+				text: 'Section',
+				items: [
+					{
+						text: 'Section Introduction',
+						link: '/section/#section-introduction'
+					},
+					{
+						text: 'Some More Content!',
+						link: '/section/#some-more-content'
+					}
+				]
+			},
+			{
+				text: 'Contact',
+				items: [
+					{
+						text: 'Twitter',
+						link: 'https://www.twitter.com/'
+					},
+					{
+						text: 'Email',
+						link: 'mailto:hello@email.com'
+					}
+				]
+			}*/
+		],
+		docsDir: 'docs'
+	}
+}
